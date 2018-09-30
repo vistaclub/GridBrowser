94f1f16bd78ec01cf5a6012e3a3c8547b127f19f
Jason Wong <jawong71@gmail.com>
2018-09-29 18:43:10 -0700

    first commit

---
+++ Project4/WindowController.swift
@@ -0,0 +1,19 @@
+//
+//  WindowController.swift
+//  Project4
+//
+//  Created by Jason Wong on 2018-08-19.
+//  Copyright © 2018 Jason Wong. All rights reserved.
+//
+
+import Cocoa
+
+class WindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+
+}
